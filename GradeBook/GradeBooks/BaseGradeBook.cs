3ca5f91b5f98bc8dd4e33f84987731e0eb73b4f8
DarrellK <darrell@clearcutcomputers.com.au>
2019-04-20 10:19:54 +0930

    Update BaseGradeBook.cs

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -20,7 +20,7 @@ namespace GradeBook.GradeBooks
         public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
-            isWeighted = isWeighted;
+            IsWeighted = isWeighted;
             Students = new List<Student>();
         }
 
